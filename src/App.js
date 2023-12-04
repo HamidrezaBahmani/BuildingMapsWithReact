7f5ab62dafa0ce6ddee7563b1a09d42bfdc660bc
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-12-04 20:01:17 +0330

    Using NavLinks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
 
 export default App;
+//routing:
+//our-domain.com/ =>Coponent A
+//our-domain.com/products =>Component B
